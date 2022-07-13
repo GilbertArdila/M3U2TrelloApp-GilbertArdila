a55c4465cb92acb41ecfb706191f600bb5e01006
Gilbert Ardila <gilbertferney@gmail.com>
2022-07-13 11:08:29 -0500

    adding sortable.css

--- src/utils/nodes.js
+++ src/utils/nodes.js
@@ -5,6 +5,9 @@ bringing all the necesary nodes from html document
 
 const taskBoard=document.querySelector(".taskBoard");
 const newTask_button=document.getElementById("newTask-button");
+const taskBoard_toDo=document.querySelector(".taskBoard-toDo");
+const taskBoard_doing=document.querySelector(".taskBoard-doing");
+const taskBoard_done=document.querySelector(".taskBoard-done");
 const todoDiv=document.querySelector(".todo");
 const doingDiv=document.querySelector(".doing");
 const doneDiv=document.querySelector(".done");
